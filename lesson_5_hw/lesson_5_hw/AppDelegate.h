58f15fbe1b32e3bbcf7db4f7bd471df1692cfc13
maxud <mlenon13@gmail.com>
2017-08-24 15:59:37 +0300

    добавил проект

---
+++ lesson_5_hw/lesson_5_hw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lesson_5_hw
+//
+//  Created by maxud on 24.08.17.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
